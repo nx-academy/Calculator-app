a66efd3b1ae817438cfe4d96a9f01f4c5292fc72
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-09-15 09:58:07 +0200

    Preparing theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
+// Background
+const FIORD = "#3A4764"
+const EBONY_CLAY = "#232C43"
+const MIRAGE = "#182034"
+
+// Keys
+const LYNCH = "#637097"
+const EAST_BAY = "#404E72"
+const PERSIAN_RED = "#D03F2F"
+const OLD_BRICK = "#93261A"
+const PEARL_BUSH = "#EAE3DC"
+const NAPA = "#B4A597"
+
+// Text
+const RIVER_BED = "#444B5A"
+const WHITE = "#FFF"
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    colors: {
+      fiord: FIORD,
+      ebonyClay: EBONY_CLAY,
+      mirage: MIRAGE
+    }
   },
   plugins: [],
 }

b036e0464a4f60c1792be1a494033b0b9bec668f
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-09-15 14:57:57 +0200

    Starting to play with operations

--- ts/main.ts
+++ ts/main.ts
@@ -1,3 +1,5 @@
+const store = {};
+
 function handleReset(): void {
   const $resetBtn = document.querySelector(".reset-btn");
 
@@ -12,23 +14,39 @@ function handleKeyNumber(): void {
 
   $keysNumber.forEach(($keyNumber) =>
     $keyNumber.addEventListener("click", function () {
-        const $result = document.querySelector('.result')
-        let currentResult = $result.textContent
+      const $result = document.querySelector(".result");
+      let currentResult = $result.textContent;
 
-        if (currentResult === "0") {
-            currentResult = $keyNumber.textContent
-        } else {
-            currentResult = currentResult + $keyNumber.textContent
-        }
-    
-        $result.textContent = currentResult
+      if (currentResult === "0") {
+        currentResult = $keyNumber.textContent;
+      } else {
+        currentResult = currentResult + $keyNumber.textContent;
+      }
+
+      $result.textContent = currentResult;
     })
   );
+}
+
+function handleKeyOperator(): void {
+  const $operators = document.querySelectorAll(".operator");
+
+  $operators.forEach(($operator) => {
+    $operator.addEventListener("click", function () {
+        const $result = document.querySelector('.result')
+        const operatorOperation = $operator.getAttribute('data-value')
+
+        console.log(operatorOperation)
+
+        $result.textContent = "0"
+    });
+  });
 }
 
 function main(): void {
   handleReset();
   handleKeyNumber();
+  handleKeyOperator();
 }
 
 main();

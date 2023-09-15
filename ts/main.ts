01a66ed07c78fb74aa454e09352dafb839dfc9af
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-09-15 12:58:31 +0200

    Starting handling key touches

--- ts/main.ts
+++ ts/main.ts
@@ -1,1 +1,34 @@
-console.log("====")
+function handleReset(): void {
+  const $resetBtn = document.querySelector(".reset-btn");
+
+  $resetBtn.addEventListener("click", function () {
+    const $result = document.querySelector(".result");
+    $result.textContent = "0";
+  });
+}
+
+function handleKeyNumber(): void {
+  const $keysNumber = document.querySelectorAll(".key");
+
+  $keysNumber.forEach(($keyNumber) =>
+    $keyNumber.addEventListener("click", function () {
+        const $result = document.querySelector('.result')
+        let currentResult = $result.textContent
+
+        if (currentResult === "0") {
+            currentResult = $keyNumber.textContent
+        } else {
+            currentResult = currentResult + $keyNumber.textContent
+        }
+    
+        $result.textContent = currentResult
+    })
+  );
+}
+
+function main(): void {
+  handleReset();
+  handleKeyNumber();
+}
+
+main();

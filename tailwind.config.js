f08f268df671cf0e9522aa8410b209bf91ca1fe3
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-10-03 16:35:20 +0200

    Still working on the theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,22 @@
 const { createThemes } = require("tw-colors");
 
 // Descriptive color variables
-const firstTheme = {
-  // Background
-  FIORD: "#3A4764",
-  EBONY_CLAY: "#232C43",
-  MIRAGE: "#182034",
+// Background
+const FIORD = "#3A4764";
+const EBONY_CLAY = "#232C43";
+const MIRAGE = "#182034";
 
-  // Keys
-  LYNCH: "#637097",
-  EAST_BAY: "#404E72",
-  PERSIAN_RED: "#D03F2F",
-  OLD_BRICK: "#93261A",
-  PEARL_BUSH: "#EAE3DC",
-  NAPA: "#B4A597",
+// Keys
+const LYNCH = "#637097";
+const EAST_BAY = "#404E72";
+const PERSIAN_RED = "#D03F2F";
+const OLD_BRICK = "#93261A";
+const PEARL_BUSH = "#EAE3DC";
+const NAPA = "#B4A597";
 
-  // Text
-  RIVER_BED: "#444B5A",
-  WHITE: "#FFF",
-};
+// Text
+const RIVER_BED = "#444B5A";
+const WHITE = "#FFF";
 
 // Functional color variables
 const theme1 = {
@@ -31,9 +29,9 @@ const theme1 = {
   "secondary-dark": OLD_BRICK,
   tertiary: PEARL_BUSH,
   "tertiary-dark": NAPA,
+  
 };
 
-const theme2 = {};
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -55,7 +53,7 @@ module.exports = {
         napa: NAPA,
         "river-bed": RIVER_BED,
         white: WHITE,
-      },
-    }),
+      }
+    })
   ],
 };

ce296ba3fab725b5e7c60aafc4f399e4a2c47e6a
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-10-13 09:52:01 +0200

    Theme 2 is not perfect but starts to look okay

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,6 @@ const descriptiveTheme1 = {
   FIORD: "#3A4764",
   EBONY_CLAY: "#232C43",
   MIRAGE: "#182034",
-
   // Keys
   LYNCH: "#637097",
   EAST_BAY: "#404E72",
@@ -14,9 +13,25 @@ const descriptiveTheme1 = {
   OLD_BRICK: "#93261A",
   PEARL_BUSH: "#EAE3DC",
   NAPA: "#B4A597",
-
   // Text
   RIVER_BED: "#444B5A",
+  WHITE: "#FFF",
+};
+
+const descriptiveTheme2 = {
+  // Background
+  MERCURY: "#E6E6E6",
+  SWISS_COFFEE: "#D1CCCC",
+  GALLERY: "#EDEDED",
+  // Keys
+  PARADISO: "#377F86",
+  BLUEMINE: "#1B5F65",
+  BURNT_ORANGE: "#CA5502",
+  PERU_TAN: "#893901",
+  WESTAR: "#E5E4E1",
+  ZORBA: "#A69D91",
+  // Text
+  ARMADILLO: "#35352C",
   WHITE: "#FFF",
 };
 
@@ -35,6 +50,20 @@ const theme1 = {
   "quarternary-light": descriptiveTheme1.WHITE,
 };
 
+const theme2 = {
+  primary: descriptiveTheme2.MERCURY,
+  "primary-dark": descriptiveTheme2.SWISS_COFFEE,
+  "primary-darker": descriptiveTheme2.GALLERY,
+  "primary-light": descriptiveTheme2.BLUEMINE,
+  "primary-lighter": descriptiveTheme2.PARADISO,
+  secondary: descriptiveTheme2.BURNT_ORANGE,
+  "secondary-dark": descriptiveTheme2.PERU_TAN,
+  tertiary: descriptiveTheme2.WESTAR,
+  "tertiary-dark": descriptiveTheme2.ZORBA,
+  quarternary: descriptiveTheme2.ARMADILLO,
+  "quarternary-light": descriptiveTheme2.WHITE,
+};
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html"],
@@ -42,6 +71,7 @@ module.exports = {
   plugins: [
     createThemes({
       "theme-1": theme1,
+      "theme-2": theme2,
     }),
   ],
 };

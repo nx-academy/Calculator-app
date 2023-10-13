01867b7150545a1b52a6667c8166315f4acce04f
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-10-13 09:36:50 +0200

    Better theme organisation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,39 @@
 const { createThemes } = require("tw-colors");
 
 // Descriptive color variables
-// Background
-const FIORD = "#3A4764";
-const EBONY_CLAY = "#232C43";
-const MIRAGE = "#182034";
+const descriptiveTheme1 = {
+  // Background
+  FIORD: "#3A4764",
+  EBONY_CLAY: "#232C43",
+  MIRAGE: "#182034",
 
-// Keys
-const LYNCH = "#637097";
-const EAST_BAY = "#404E72";
-const PERSIAN_RED = "#D03F2F";
-const OLD_BRICK = "#93261A";
-const PEARL_BUSH = "#EAE3DC";
-const NAPA = "#B4A597";
+  // Keys
+  LYNCH: "#637097",
+  EAST_BAY: "#404E72",
+  PERSIAN_RED: "#D03F2F",
+  OLD_BRICK: "#93261A",
+  PEARL_BUSH: "#EAE3DC",
+  NAPA: "#B4A597",
 
-// Text
-const RIVER_BED = "#444B5A";
-const WHITE = "#FFF";
+  // Text
+  RIVER_BED: "#444B5A",
+  WHITE: "#FFF",
+};
 
 // Functional color variables
 const theme1 = {
-  primary: FIORD,
-  "primary-dark": EBONY_CLAY,
-  "primary-darker": MIRAGE,
-  "primary-light": EAST_BAY,
-  "primary-lighter": LYNCH,
-  secondary: PERSIAN_RED,
-  "secondary-dark": OLD_BRICK,
-  tertiary: PEARL_BUSH,
-  "tertiary-dark": NAPA,
-  
+  primary: descriptiveTheme1.FIORD,
+  "primary-dark": descriptiveTheme1.EBONY_CLAY,
+  "primary-darker": descriptiveTheme1.MIRAGE,
+  "primary-light": descriptiveTheme1.EAST_BAY,
+  "primary-lighter": descriptiveTheme1.LYNCH,
+  secondary: descriptiveTheme1.PERSIAN_RED,
+  "secondary-dark": descriptiveTheme1.OLD_BRICK,
+  tertiary: descriptiveTheme1.PEARL_BUSH,
+  "tertiary-dark": descriptiveTheme1.NAPA,
+  quarternary: descriptiveTheme1.RIVER_BED,
+  "quarternary-light": descriptiveTheme1.WHITE,
 };
-
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -39,21 +41,7 @@ module.exports = {
   theme: {},
   plugins: [
     createThemes({
-      "theme-1": {
-        primary: FIORD,
-        "primary-dark": EBONY_CLAY,
-        "primary-darker": MIRAGE,
-        "primary-light": EAST_BAY,
-        "primary-lighter": LYNCH,
-        secondary: PERSIAN_RED,
-        "secondary-dark": OLD_BRICK,
-        tertiary: PEARL_BUSH,
-        "tertiary-dark": NAPA,
-        "pearl-bush": PEARL_BUSH,
-        napa: NAPA,
-        "river-bed": RIVER_BED,
-        white: WHITE,
-      }
-    })
+      "theme-1": theme1,
+    }),
   ],
 };
